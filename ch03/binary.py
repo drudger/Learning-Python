fc539f00b74dcc14ad129cdad397cd3405239e4b
Chris Hinds <drudger@users.noreply.github.com>
2016-11-06 09:20:31 +0000

    Finished Chapter 3

---
+++ ch03/binary.py
@@ -0,0 +1,10 @@
+n = 39
+remainders = []
+while n > 0:
+    remainder = n % 2 # remainder of division by 2
+    remainders.append(remainder) # we keep tack of remainders
+    n //= 2 # we divide n by 2
+
+# reassign the list to its reversed copy and print it
+remainders = remainders[::-1]
+print(remainders)
